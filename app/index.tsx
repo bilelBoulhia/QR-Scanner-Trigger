fab3c1e1a02a93c6f492fdd60136427a1fb93b8f
bilelBoulhia <billel.boulahia456@gmail.com>
2024-11-23 20:52:26 +0100

    init project

---
+++ app/index.tsx
@@ -0,0 +1,156 @@
+import {View, Text, TouchableOpacity, Platform, Animated, Button} from "react-native";
+import * as WebBrowser from 'expo-web-browser';
+import { StyleSheet } from "react-native";
+import * as Google from 'expo-auth-session/providers/google';
+import {useEffect} from "react";
+import {LinearGradient} from "expo-linear-gradient";
+import {Ionicons} from "@expo/vector-icons";
+import {useToken} from "../context/authProvider";
+import {router} from "expo-router";
+const webClientId = '623858970830-s2tnvsvmo2jvo921mdpenpug5b3k07a6.apps.googleusercontent.com'
+const androidClientId = '623858970830-2qloc0ubuusb7vd4jvhg0s0nbblfa19c.apps.googleusercontent.com'
+
+WebBrowser.maybeCompleteAuthSession();
+
+
+
+export default function  Page(){
+    const config = {
+        clientId: Platform.select({
+            android: androidClientId,
+
+            default: webClientId,
+        }),
+        scopes: [
+            'https://www.googleapis.com/auth/drive.metadata.readonly',
+            'https://www.googleapis.com/auth/drive.readonly',
+        ],
+    };
+
+    const { setToken } = useToken();
+    const [request,response,promptAsync] = Google.useAuthRequest(config)
+
+    const handleToken =async () => {
+        if (response?.type === "success") {
+            const token = response.authentication?.accessToken;
+            if (token) {
+                setToken(token);
+                router.push('/sheets');
+                console.log("Token:", token);
+            } else {
+                console.error("Failed to retrieve the token");
+            }
+        } else {
+            console.error("Authentication response type is not 'success'");
+        }
+    };
+
+
+    const tempPushing= ()=>{
+        router.push('/test');
+    }
+
+
+    const fadeAnim = new Animated.Value(0);
+    const slideAnim = new Animated.Value(50);
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 10,
+                useNativeDriver: true,
+            }),
+            Animated.spring(slideAnim, {
+                toValue: 0,
+                friction: 4,
+                tension: 10,
+                useNativeDriver: true,
+            }),
+        ]).start();
+        if (response) {
+            handleToken();
+        }
+    }, [response]);
+
+
+    return (
+        <View style={{gap:25,flex:1,justifyContent:'center',alignItems:'center'}}>
+            <View style={styles.textContainer}>
+                <Ionicons name="qr-code-outline" size={80} color="#333" style={styles.icon} />
+                <Text style={styles.bigText}>QR</Text>
+
+            </View>
+            <TouchableOpacity onPress={()=>promptAsync()} style={styles.button}>
+                <LinearGradient
+                    colors={['#60A5FA', '#818CF8', '#A78BFA']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={styles.gradient}
+                >
+                    <View style={styles.contentContainer}>
+
+                        <Text style={styles.text}>Sign in with </Text>
+                        <View>
+                            <Ionicons name="logo-google" size={24} color="white" />
+                        </View>
+                    </View>
+                </LinearGradient>
+                <Button title='test' onPress={tempPushing}/>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+
+    textContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+
+
+        gap: 10,
+    },
+
+    icon: {
+
+    },
+    bigText: {
+        fontFamily: 'Roboto',
+        fontWeight: 'bold',
+        fontSize: 72,
+        color: '#333',
+        letterSpacing: -2,
+    },
+    button: {
+        borderRadius: 4,
+        overflow: 'hidden',
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+    },
+    gradient: {
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+    },
+    contentContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        width:150,
+        gap:10
+
+    },
+
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
---
+++ app/sheets.tsx
@@ -0,0 +1,236 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
+import { useToken } from "../context/authProvider";
+import {Redirect,  useRootNavigationState, useRouter} from "expo-router";
+import {LinearGradient} from "expo-linear-gradient";
+
+
+
+
+
+const LoadingSpinner = () => {
+    return (
+        <View style={styles.centerContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+    )
+}
+
+const Retry = ({error ,fn}:{error:string,fn:Promise<void>})=>{
+    return(
+        <View style={styles.centerContainer}>
+            <Text style={styles.errorText}>{error}</Text>
+            <TouchableOpacity style={styles.retryButton} onPress={() => fn}>
+                <Text style={styles.retryButtonText}>Retry</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default function Page() {
+    const rootNavigationState = useRootNavigationState();
+    const {token, setToken} = useToken();
+    const [spreadsheets, setSpreadsheets] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        if (token) {
+            fetchSpreadsheets(token).catch(error => console.error(error));
+        } else {
+            setIsLoading(false);
+        }
+    }, [token]);
+
+    const fetchSpreadsheets = async (token:string) => {
+        if (!token) {
+            setError("Token is missing. Please authenticate first.");
+            setIsLoading(false);
+            return;
+        }
+
+        try {
+            const response = await fetch(
+                'https://www.googleapis.com/drive/v3/files?' +
+                new URLSearchParams({
+                    q: "mimeType='application/vnd.google-apps.spreadsheet'",
+                    fields: 'files(id,name)',
+                    orderBy: 'modifiedTime desc',
+                }).toString(),
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = await response.json();
+            setSpreadsheets(data.files || []);
+            setIsLoading(false);
+        } catch (err) {
+            setError("Failed to fetch spreadsheets. Please try again.");
+            setIsLoading(false);
+        }
+    };
+
+    const handleLogout = () => {
+        setToken(null);
+        router.replace('/');
+    };
+
+    const router = useRouter();
+    const handleRouting = (id:string) => {
+
+        router.push({
+            pathname: `/${id}`,
+            params: { id:id }
+        });
+    }
+
+    const renderItem = ({ item }) => (
+        <TouchableOpacity style={styles.item} onPress={()=> handleRouting(item.id)}>
+            <Text style={styles.itemText}>{item.name}</Text>
+        </TouchableOpacity>
+    );
+
+    switch (true) {
+        case !rootNavigationState?.key:
+            return null;
+
+        case token === null:
+            return <Redirect href={'/'}/>;
+
+        case isLoading:
+            return <LoadingSpinner/>;
+
+        case !!error:
+            return <Retry error={error} fn={fetchSpreadsheets(token)}/>;
+
+        default:
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.title}>Your Spreadsheets</Text>
+                    {spreadsheets && spreadsheets.length > 0 ? (
+                        <FlatList
+                            data={spreadsheets}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.id}
+                            contentContainerStyle={styles.listContent}
+                        />
+                    ) : (
+                        <Text style={styles.noSpreadsheetsText}>
+                            {spreadsheets === null ? "Failed to load spreadsheets" : "No spreadsheets found"}
+                        </Text>
+                    )}
+
+                    <TouchableOpacity onPress={handleLogout} style={styles.button}>
+                        <LinearGradient
+                            colors={['#60A5FA', '#818CF8', '#A78BFA']}
+                            start={{x: 0, y: 0}}
+                            end={{x: 1, y: 0}}
+                            style={styles.gradient}
+                        >
+
+                            <Text style={styles.text}>Log out </Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </View>
+            );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        padding: 20,
+        flexDirection: 'column',
+
+    },
+    centerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+    },
+
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        alignSelf:'center'
+    },
+    gradient: {
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#333',
+    },
+    listContent: {
+        paddingBottom: 20,
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    itemText: {
+        fontSize: 18,
+        color: '#333',
+    },
+    button: {
+        borderRadius: 4,
+        overflow: 'hidden',
+        elevation: 3,
+        width:150,
+        textAlign: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        alignSelf:'center'
+
+    },
+    logoutButtonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    loadingText: {
+        marginTop: 10,
+        fontSize: 16,
+        color: '#333',
+    },
+    errorText: {
+        fontSize: 16,
+        color: '#ff0000',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    retryButton: {
+        backgroundColor: '#4CAF50',
+        padding: 10,
+        borderRadius: 5,
+    },
+    retryButtonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    noSpreadsheetsText: {
+        fontSize: 16,
+        color: '#666',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+});
+
